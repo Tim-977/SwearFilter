b9fad175be6de47d59beebcaafa2f76d08f2766d
Tim977 <gentim977@gmail.com>
2023-07-30 01:48:44 -0400

    Update words.py

--- words.py
+++ words.py
@@ -1,5 +1,84 @@
-from utils import valuedWords, words
+import json
 
+with open('valuedWords.txt', 'r', encoding='UTF-8') as f:
+    valuedWords = f.readlines()
+    valuedWords = [
+        x
+        for x in [x[:-1] if x != valuedWords[-1] else x for x in valuedWords]
+        if x
+    ]
+
+with open('result.json', 'r', encoding='UTF-8') as json_file:
+    data = json.load(json_file)
+
+with open('good_list.txt', "r", encoding="utf-8") as file:
+    content = file.read()
+
+wordss = content.split()
+good_words = wordss
+
+
+
+messages_texts = []
+
+for message in data['messages']:
+    messages_texts.append(message['text'])
+
+words = []
+
+
+def add_words(text):
+    words.extend(text.split())
+
+
+for sentence in messages_texts:
+    if isinstance(sentence, str):
+        add_words(sentence)
+    elif isinstance(sentence, list):
+        for item in sentence:
+            if isinstance(item, str):
+                add_words(item)
+            elif isinstance(item, dict) and 'text' in item:
+                add_words(item['text'])
+
+
+def detect_spam(text):
+    text = text.lower()
+    if 'аааа' in text or 'аха' in text or 'яяя' in text or 'f[f' in text or 'axa' in text or 'еее' in text or 'ойой' in text or 'оооо' in text or 'блябля' in text or 'feulnaim' in text or 'спасспас' in text or 'пжпжпжпж' in text or 'ээээ' in text or 'susalarm' in text or 'мммм' in text or 'аахх' in text or 'http' in text or 'pрууp' in text or 'kфklkфsфоф' in text or 'дvхfvpsfcхs' in text or 'шзолдюиа' in text or 'sxdvgrextpa' in text or 'уууу' in text or 'фjъaячъ' in text or 'ййй' in text or len(text) > 23:
+        return True
+    return False
+
+
+def remove_non_alpha(word):
+    return ''.join(char for char in word if char.isalpha())
+
+
+spam = []
+temp_words = []
+
+parsed_words = [remove_non_alpha(word) for word in words]
+
+givenWords = list(filter(None, parsed_words))
+
+givenWords = [x.lower() for x in givenWords[:]]
+
+for word in givenWords:
+    if not detect_spam(word):
+        temp_words.append(word)
+    else:
+        spam.append(word)
+
+givenWords = temp_words[:]
+
+maxx, maxelem = 0, ''
+
+for elem in givenWords:
+    if len(elem) > maxx:
+        maxx = len(elem[:])
+        maxelem = elem[:]
+
+print(maxx)
+print(maxelem)
 
 def levenshtein_distance(s, t):
     m = len(s)
@@ -23,6 +102,7 @@ def correct_spelling(word, valued_words):
         closest_word = ''
     else:
         distances = [(levenshtein_distance(word, w), w) for w in valued_words]
+        # print(sorted(distances))
         closest_word = min(distances)[1]
         handled_closest = f"{word} -- {closest_word} -- {min(distances)[0]}/{len(closest_word)} -- {int((round((min(distances)[0] / len(closest_word)) * 100, 0)))}%"
         handled_word = f"{word} -- {min(distances)[0]}/{len(closest_word)} -- {int((round((min(distances)[0] / len(closest_word)) * 100, 0)))}%    ----- {closest_word}"
@@ -32,17 +112,19 @@ def correct_spelling(word, valued_words):
 
 changed_list = []
 original_list = []
+
 counter = 1
-for word in words:
+
+for word in givenWords:
     temp = correct_spelling(word, valuedWords)
-    if temp[1]:
+    if temp[1] or word in good_words:
         original_list.append(temp[0])
     else:
         changed_list.append(temp[0])
     
     if counter % 100 == 0:
-        print(f'{counter} {"▮" * int(str((counter / len(words)) * 100)[:1])}{"▯" * (10 - int(str((counter / len(words)) * 100)[:1]))} {str((counter / len(words)) * 100)[:5]}%')
-    if counter == len(words):
+        print(f'{counter} {"▮" * int(str((counter / len(givenWords)) * 100)[:1])}{"▯" * (10 - int(str((counter / len(givenWords)) * 100)[:1]))} {str((counter / len(givenWords)) * 100)[:5]}% {word}')
+    if counter == len(givenWords):
         print(f'{counter} "▮▮▮▮▮▮▮▮▮▮ 100%')
     counter += 1
 
@@ -60,8 +142,16 @@ with open('changed_list.txt', 'w', encoding='UTF-8') as file:
         file.write(elem + '\n')
     file.write(str(len(changed_list)))
 
-print(f'Changed length: {len(changed_list)}')
+with open('spam.txt', 'w', encoding='UTF-8') as file:
+    file.write('Spam list\n')
+    for elem in spam:
+        file.write(elem + '\n')
+    file.write(str(len(spam)))
 
 print(f'Original length: {len(original_list)}')
 
 print(f'Swear: {len(changed_list)}')
+
+print(f'Swear_%: {len(changed_list) / len(original_list)}')
+
+print(f'Spam: {len(spam)}')
